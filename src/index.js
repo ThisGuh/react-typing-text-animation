a564d18d4cce9aff4470061e1b11ece27c5bf55f
thisGuh <jakiepolska1111@gmail.com>
2021-04-05 18:54:46 +0200

    Clean up and install prop-types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+function TypingTextAnimation({ children, className }) {
+  return <div className={className}>{children}</div>
 }
+
+export default TypingTextAnimation
