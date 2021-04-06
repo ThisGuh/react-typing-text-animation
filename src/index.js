eb692ed8b5ce06900a9ac72de32c3d733c8e4d07
thisGuh <jakiepolska1111@gmail.com>
2021-04-06 17:22:56 +0200

    add prop-types and cursor props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Cursor from './components/Cursor'
 
-function TypingTextAnimation({ children, className, cursor }) {
-  return <div className={className}>{children}</div>
+function TypingTextAnimation({
+  children,
+  className,
+  cursorDuration,
+  cursor = <Cursor cursorDuration={cursorDuration} />
+}) {
+  return (
+    <div className={className}>
+      {children}
+      {cursor}
+    </div>
+  )
 }
 
 export default TypingTextAnimation
+
+TypingTextAnimation.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  cursorDuration: PropTypes.string
+}
