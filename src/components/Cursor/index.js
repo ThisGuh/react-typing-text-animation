9c61ee88596f02e60f196791f6384d97dc647ed2
thisGuh <jakiepolska1111@gmail.com>
2021-04-24 21:08:34 +0200

    delete default prop value

--- src/components/Cursor/index.js
+++ src/components/Cursor/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../../styles.module.css'
 
-function Cursor({ cursorDuration = '1s' }) {
+function Cursor({ cursorDuration }) {
   const cursorDurationStyle = {
     animationDuration: cursorDuration
   }
