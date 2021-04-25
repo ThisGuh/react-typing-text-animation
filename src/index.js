25c93ecd66066b5e2bd2c05e3ae2a831660e381e
thisGuh <jakiepolska1111@gmail.com>
2021-04-25 15:58:45 +0200

    add reverse brake

--- src/index.js
+++ src/index.js
@@ -9,36 +9,41 @@ function TypingTextAnimation({
   cursorDuration,
   reverse,
   delayOnReverse,
+  brakeBeforeReverse,
   cursor = <Cursor cursorDuration={cursorDuration} />
 }) {
   const [wordIndex, setWordIndex] = useState(0)
   const [typedWord, setTypedWord] = useState('')
   const [isReverseDirection, setReverseDirection] = useState(false)
+  const [typingDelay, setTypingDelay] = useState(delay)
 
   useEffect(() => {
     if (wordIndex === text.length && !reverse) return
 
     if (wordIndex === text.length && reverse) {
       setReverseDirection(true)
+      setTypingDelay(brakeBeforeReverse)
+    }
+
+    if (wordIndex === text.length - 1 && isReverseDirection) {
+      setTypingDelay(delayOnReverse)
     }
 
     if (wordIndex === 0) {
       setReverseDirection(false)
+      setTypingDelay(delay)
     }
 
-    const timeout = setTimeout(
-      () => {
-        const letter = text.substr(wordIndex, 1)
+    const timeout = setTimeout(() => {
+      const letter = text.substr(wordIndex, 1)
 
-        setWordIndex((prevIndex) =>
-          isReverseDirection ? prevIndex - 1 : prevIndex + 1
-        )
-        setTypedWord((prev) =>
-          isReverseDirection ? prev.slice(0, -1) : prev + letter
-        )
-      },
-      isReverseDirection ? delayOnReverse : delay
-    )
+      setWordIndex((prevIndex) =>
+        isReverseDirection ? prevIndex - 1 : prevIndex + 1
+      )
+      setTypedWord((prev) =>
+        isReverseDirection ? prev.slice(0, -1) : prev + letter
+      )
+    }, typingDelay)
 
     return () => clearTimeout(timeout)
   }, [wordIndex, typedWord])
@@ -60,6 +65,7 @@ TypingTextAnimation.propTypes = {
   cursorDuration: PropTypes.string,
   reverse: PropTypes.bool,
   delayOnReverse: PropTypes.number,
+  brakeBeforeReverse: PropTypes.number,
   cursor: PropTypes.element
 }
 
@@ -69,5 +75,6 @@ TypingTextAnimation.defaultProps = {
   delay: 200,
   cursorDuration: '1s',
   reverse: false,
-  delayOnReverse: 100
+  delayOnReverse: 50,
+  brakeBeforeReverse: 2500
 }
